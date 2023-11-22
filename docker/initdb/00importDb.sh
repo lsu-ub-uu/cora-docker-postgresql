18f86a3bcbcca2129fa426f4ded78166b1b15926
palle <palle.raabjerg@ub.uu.se>
2023-11-22 11:12:59 +0100

    Fix init script so it sets the password via PGPASSWORD

--- docker/initdb/00importDb.sh
+++ docker/initdb/00importDb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
+PGPASSWORD=$POSTGRES_PASSWORD
 
 function run_sql () {
    for SQL in "$1"/*
 	do
  	 echo "Run file: $SQL"
-  		psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER $POSTGRES_PASSWORD < $SQL > $SQL.log
+		psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER $POSTGRES_DB < $SQL > $SQL.log
 	done
 }
 
